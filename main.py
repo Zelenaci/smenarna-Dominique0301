23a26deeb6e7d542c1e80abd287ce446a3211373
Dominik Šlehofer <slehoferdominik@gmail.com>
2022-03-05 16:59:11 +0100

    validace vstupu

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from tkinter import Listbox, END
 #from typing_extensions import IntVar
 from tkinter import IntVar, messagebox
 
+from numpy import True_
+
 # from tkinter import ttk
 
 
@@ -23,7 +25,7 @@ class About(tk.Toplevel):
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "Směnárna"
 
     def __init__(self):
         self.index = None
@@ -33,26 +35,31 @@ class Application(tk.Tk):
         self.v = IntVar()
 
         self.prodej = tk.Radiobutton(self, text="prodej", variable=self.v, value=1, command=self.prodej)
-        self.prodej.pack()
+        self.prodej.grid(row=1, column=1)
 
         self.nakup = tk.Radiobutton(self, text="nákup", variable=self.v, value=2, command=self.nakup)
-        self.nakup.pack()
+        self.nakup.grid(row=2, column=1)
 
-        self.vstup = tk.Entry(self)
-        self.vstup.pack()
+        self.vstup = tk.Entry(self, validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.vstup.grid(row=3, column=1)
 
         self.lbl_vysledek =tk.Label(self, text="")
-        self.lbl_vysledek.pack()
+        self.lbl_vysledek.grid(row=4, column=1)
+
+        self.lbl_mena = tk.Label(self, text="CZK")
+        self.lbl_mena.grid(row=4, column=2)
 
         self.lstBx = Listbox(self)
-        self.lstBx.pack()
+        self.lstBx.grid(row=5, column=1)
         self.lstBx.bind("<ButtonRelease-1>", self.kliknu)
+        self.geometry("400x500")
 
         f = open("listek.txt")
         self.radky = f.readlines()
 
         self.prodejni_ceny = []
         self.nakupni_ceny = []
+       
 
         for radek in self.radky:
             radek = radek.split()
@@ -69,27 +76,37 @@ class Application(tk.Tk):
 
     def prodej(self):
         if self.index == None or self.vstup.get() == "":
-            messagebox.showwarning("Chyba","Nezadali jste vstupní hodnotu!")
+            messagebox.showwarning("Chyba","Nezadali jste vstupní hodnotu nebo nevybrali měnu!")
         else:
-            self.p = int(self.vstup.get())*float(self.prodejni_ceny[self.index])
+            self.p = float(self.vstup.get())*float(self.prodejni_ceny[self.index])
             self.lbl_vysledek.config(text=self.p)
 
 
     def nakup(self):
         if self.index == None or self.vstup.get() == "":
-            messagebox.showwarning("Chyba", "Nezadali jste vstupní hodnotu!")
+            messagebox.showwarning("Chyba", "Nezadali jste vstupní hodnotu nebo nevybrali měnu!")
         else:
-            self.n = int(self.vstup.get())*float(self.nakupni_ceny[self.index])
+            self.n = float(self.vstup.get())*float(self.nakupni_ceny[self.index])
             self.lbl_vysledek.config(text=self.n)
 
+    
+    
+    def desetinne(self):
+        try:
+            float(self.vstup.get())
+        except:
+            return False
+        return True
 
         
-        
+    def validate(self, value):
+        if len(value) == 0 or value.isnumeric() or self.desetinne():
+            return True
+        else:
+            return False
 
 
-    def about(self):
-        window = About(self)
-        window.grab_set()
+  
 
     def quit(self, event=None):
         super().quit()
